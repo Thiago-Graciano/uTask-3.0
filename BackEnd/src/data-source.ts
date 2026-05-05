77b1b9f0623afc940cc414e4b9cd85ec59678e9b
Thiago Graciano <msgraciano28@gmail.com>
2026-05-05 00:01:57 -0300

    feat: initial project structure (backend + frontend)

--- BackEnd/src/data-source.ts
+++ BackEnd/src/data-source.ts
@@ -1,17 +1,17 @@
 import "reflect-metadata";
+import "dotenv/config";
 import { DataSource } from "typeorm";
 import { User } from "./entities/User.js";
 import { Task } from "./entities/Task.js";
 
-
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres", 
-    password: "dark1213",
-    database: "utask_db", 
-    synchronize: true,  
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT!),
+    username: process.env.DB_USER!,
+    password: process.env.DB_PASS!,
+    database: process.env.DB_NAME!,
+    synchronize: true,
     logging: false,
     entities: [User, Task],
     migrations: [],
